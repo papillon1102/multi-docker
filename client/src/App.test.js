02605a9dc99d4d9afe0d38359edbc6af19e8b887
A-Lu <macbook@Macbooks-MacBook-Pro-2.local>
2021-11-08 01:43:31 +0700

    changed travis yml and App.test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -3,3 +3,6 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {});
+
+// script:
+//   - docker run -e CI=true USERNAME/react-test npm test
