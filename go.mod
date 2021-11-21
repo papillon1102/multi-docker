a1dffa67eede596ccc4e2cd302b8d430d81761f9
A-Lu <macbook@Macbooks-MacBook-Pro-2.local>
2021-11-21 12:12:30 +0700

    list and add tasks endpoint

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/papillon1102/go-tasks
 
 go 1.16
 
-require github.com/gin-gonic/gin v1.7.4
+require (
+	github.com/gin-gonic/gin v1.7.4
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/rs/xid v1.3.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,8 +1,54 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+	"time"
 
-func main() {
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
+
+var tasks []Task
+
+type User struct {
+	Name  string `json:"name"`
+	GGID  string `json:"ggid"`
+	Email string `json:"email"`
+}
+
+type Task struct {
+	ID        string    `json:"id"`
+	Name      string    `json:"name"`
+	Tags      []string  `json:"tags"`
+	User      User      `json:"user"`
+	Status    string    `json:"status"`
+	CreatedAt time.Time `json:createdAt`
+}
+
+func NewTaskHandler(c *gin.Context) {
+	var task Task
+
+	// Decode request body in to "Task" struct
+	if err := c.ShouldBindJSON(&task); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	task.ID = uuid.NewString()
+	task.CreatedAt = time.Now()
+
+	tasks = append(tasks, task)
+	c.JSON(http.StatusOK, task)
+}
+
+func ListTaskHandler(c *gin.Context) {
+
+	// Encode "tasks" array into JSON
+	c.JSON(http.StatusOK, tasks)
+}
+
+func NewRouter() *gin.Engine {
 	router := gin.Default()
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
@@ -10,5 +56,12 @@ func main() {
 		})
 	})
 
-	router.Run()
+	router.POST("/task", NewTaskHandler)
+	router.GET("/task", ListTaskHandler)
+	return router
+}
+
+func main() {
+	r := NewRouter()
+	r.Run()
 }
